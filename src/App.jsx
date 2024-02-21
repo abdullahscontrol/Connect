231a4e2d2ac82fc4e95bc9fc2653d30f741aa551
abdullah <mariyahassangaya12@gmail.com>
2024-02-21 09:24:24 +0100

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import Header from "./header"
-import Advertise from "../ads"
-import Topic from "../topic"
+
 function App() {
   return (
     <div>
-      <Header />
-      <Advertise />
-      <Topic />
+      <div className="hero">
+        {/* // <img src="background.jpg" alt="" /> */}
+      </div>
+      <div className="sections">
+        <div className="section">
+          <h1>Connect</h1>
+          <p>Connect with anyone, anywhere. All their links,
+            one click away." (Simple and direct)</p>
+        </div>
+        <div className="section">
+          <form action="">
+            <div className="inputs">
+              <input type="text" placeholder="Email" />
+              <input type="text" placeholder="Password" />
+            </div>
+            <input type="submit" log id="submit" />
+          </form>
+
+        </div>
+      </div>
+      <div className="sections">
+        <div className="section">
+
+        </div>
+        <div className="section"></div>
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
